d4f79f980816aa462d1681783cd6cadc7782a799
Vishal Yadav <vishal.yadav@fretron.com>
2021-10-23 19:17:08 +0530

    feat(contest): add solution - Max No Distinct Elements - CodeChef Snack Down 2021 - Qualifiers

--- src/main/java/codechef/snackdown_2021/MaxNoDistinctElements.java
+++ src/main/java/codechef/snackdown_2021/MaxNoDistinctElements.java
@@ -1,2 +1,32 @@
-package codechef.snackdown_2021;public class MaxNoDistinctElements {
+package codechef.snackdown_2021;
+
+import java.util.*;
+
+public class MaxNoDistinctElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0){
+            int N = scanner.nextInt();
+            int[] B = new int[N];
+            for (int i = 0; i < N; i++) {
+                B[i] = scanner.nextInt();
+            }
+            Arrays.sort(B);
+            int k = 0;
+            for (int i = 0; i < N; i++) {
+                if (B[i] > k){
+                    B[i] = k;
+                    k++;
+                }else if (B[i] == k){
+                    B[i] = k;
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                System.out.print(B[i] + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
 }
